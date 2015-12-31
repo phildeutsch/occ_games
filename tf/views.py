8f34e6f3a48c8433845b3863f9775cd4ca617088
Amr El-Labban <amr.ellabban@gmail.com>
2015-12-31 08:49:09 +0000

    Adding players now works

--- tf/views.py
+++ tf/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect
 from .models import TfMatch
 from .forms import TfPlayerForm
 
 # Create your views here.
 def index(request):
     matches = TfMatch.objects.order_by('played_date')[:5]
-    new_player_form = TfPlayerForm()
-    return render(request, "tf/index.html", {'matches': matches, 'form': new_player_form})
+    return render(request, "tf/index.html", {'matches': matches})
+
+def player_new(request):
+    if request.method == "POST":
+        new_player_form = TfPlayerForm(request.POST)
+        if new_player_form.is_valid():
+            player = new_player_form.save(commit=False)
+            player.save()
+            return redirect('index')
+    else:
+        new_player_form = TfPlayerForm()
+    return render(request, 'tf/add_player.html', {'form': new_player_form})
