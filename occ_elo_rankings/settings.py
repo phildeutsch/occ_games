ce6daaa0de203ba9e1821657c112b69c8eb857b8
Amr El-Labban <amr.ellabban@gmail.com>
2015-12-31 13:48:49 +0000

    Heroic static files fix

--- occ_elo_rankings/settings.py
+++ occ_elo_rankings/settings.py
@@ -121,7 +121,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
@@ -137,3 +136,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
--- occ_elo_rankings/wsgi.py
+++ occ_elo_rankings/wsgi.py
@@ -10,11 +10,11 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "occ_elo_rankings.settings")
 
 application = get_wsgi_application()
-
-from dj_static import Cling
-
+application = DjangoWhiteNoise(application)
 application = Cling(get_wsgi_application())
