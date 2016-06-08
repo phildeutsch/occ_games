d3af8abc178097f827bc59b2c233e92a69e3ddf2
Philipp Deutsch <philipp.g.deutsch@gmail.com>
2016-06-08 14:04:23 +0100

    Add test cases

--- tf/tests.py
+++ tf/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import TfMatch, TfPlayer, TfTeam
+from .models import elo_change
 
-# Create your tests here.
+# model tests
+class EloChange(TestCase):
+
+    p1 = TfPlayer(first_name='Test', last_name='Player 1')
+
+    elo_change(p1, 1000, 3, 2, k=32)
