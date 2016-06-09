35399fcba4d2b854d38b947fe4d90ece24d2c6f4
Philipp Deutsch <philipp.g.deutsch@gmail.com>
2016-06-09 14:22:41 +0100

    typo

--- occ_elo_rankings/settings.py
+++ occ_elo_rankings/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'd94l1v7u7g00n',
         'USER': 'yxosccblbhfdcy',
-        'PASSWORD': 'ymHB7aa5xYg22jt8Fd2e3SIgi',
+        'PASSWORD': '-ymHB7aa5xYg22jt8Fd2e3SIgi',
         'HOST': 'ec2-54-204-8-138.compute-1.amazonaws.com',
         'PORT': '5432',
     }
