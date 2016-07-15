8c4491062a96e8a853501d027861bada33464a5f
Philipp Deutsch <philipp.g.deutsch@gmail.com>
2016-07-15 15:01:50 +0100

    Start linking users to players

--- fix_everything.py
+++ fix_everything.py
@@ -3,11 +3,15 @@
 import config
 from tf import models
 
+# Set default ELO for each player
 players = models.TfPlayer.objects.all()
 for p in players:
     p.player_elo = config.DEFAULT_ELO
     p.save()
 
+# Go through all matches and updated elOs
 matches = models.TfMatch.objects.order_by('played_date').all()
 for m in matches:
-    m.update_elos(debug=True)
+    m.update_elos(debug=False)
+
+# Make sure each user has a player linked to them
