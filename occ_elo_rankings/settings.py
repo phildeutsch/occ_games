d3d212c54b6f638f19da001c1e809a2debe4d5a9
Philipp Deutsch <philipp.g.deutsch@gmail.com>
2016-07-17 22:17:45 +0100

    Got email working

--- occ_elo_rankings/settings.py
+++ occ_elo_rankings/settings.py
@@ -90,7 +90,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'ocandctf@gmail.com'
-EMAIL_HOST_PASSWORD = 'occtf123'
+EMAIL_HOST_PASSWORD = 'fgjysvgdxthejtup'
 DEFAULT_FROM_EMAIL = 'ocandctf@gmail.com'
 EMAIL_PORT = 587
 
