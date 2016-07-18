0db7dd6f95e1bd9ef6f2f412f316117f45fd5454
Philipp Deutsch <philipp.g.deutsch@gmail.com>
2016-07-18 12:09:09 +0100

    Clean uo

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^tf/', include('tf.urls')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^autocomplete/', include('autocomplete_light.urls')),
-]
