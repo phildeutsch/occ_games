533d5b91c515d134ca22c7791b0a952e7a18dd96
Philipp Deutsch <philipp.g.deutsch@gmail.com>
2016-08-17 14:49:58 +0100

    basic bar chart

--- occ_elo_rankings/static/tf/js/eloplot.js
+++ occ_elo_rankings/static/tf/js/eloplot.js
@@ -1,11 +1,31 @@
-var dataset = [ 25, 7, 5, 26, 11, 8, 25, 14, 23, 19,
-                14, 11, 22, 29, 11, 13, 12, 17, 18, 10,
-                24, 18, 25, 9, 3 ];d3.select("#eloplot").selectAll("div")
+var dataset = [ 5, 10, 15, 20, 25, 12, 12 ,42, 12, 13 ];
+var barPadding = 2;
+
+//Width and height
+var w = 500;
+var h = 50;
+
+var svg = d3.select("#eloplot")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
+
+svg.selectAll("rect")
     .data(dataset)
     .enter()
-    .append("div")
-    .attr("class", "bar")
-    .style("height", function(d) {
-        var barHeight = d * 5;  //Scale up by factor of 5
-        return barHeight + "px";
-    });
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", function(d) {
+        return h - d;  //Height minus data value
+    })
+    .attr("width", w / dataset.length - barPadding)
+    .attr("height", function(d) {
+        return d;  //Just the data value
+    })
+    .attr("x", function(d, i) {
+      return i * (w / dataset.length);
+      })
+    .attr("height", function(d) {
+      return d;
+      })
+    .attr("fill", "teal");
