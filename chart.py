7c5bffd6c334b314473381e227035170938773b6
Philipp Deutsch <philipp.g.deutsch@gmail.com>
2016-08-19 16:52:46 +0100

    first barchart is working

--- chart.py
+++ chart.py
@@ -23,11 +23,20 @@ p11 = [player == m.teams.order_by('id')[0].players.order_by('id')[0] for m in tf
 t2  = [player in m.teams.order_by('id')[1].players.all() for m in tf_matches]
 p21 = [player == m.teams.order_by('id')[1].players.order_by('id')[0] for m in tf_matches]
 
-elo11 = elo12 = elo21 = elo22 = []
-p11 = p12 = p21 = p22 = []
+id = []
+elo11 = []
+elo12 = []
+elo21 = []
+elo22 = []
+p11 = []
+p12 = []
+p21 = []
+p22 = []
+win = []
 
-for m in [tf_matches[0]]:
-    print(m.id)
+for m in tf_matches:
+    id.append(m.id)
+    win.append(player in m.get_winner().players.all())
     elo11.append(m.team1_elos_to_int()[0])
     elo21.append(m.team2_elos_to_int()[0])
     p11.append(m.teams.order_by('id').all()[0].players.order_by('id')[0].full_name)
@@ -45,37 +54,41 @@ for m in [tf_matches[0]]:
         elo22.append(0)
         p22.append('NA')
 
-df = pd.DataFrame({'date':dates,
-'elo11': elo11,
-'elo12': elo12,
-'elo21': elo21,
-'elo22': elo22,
-'p11'  : p11,
-'p12'  : p12,
-'p21'  : p21,
-'p22'  : p22
-})
+df = pd.DataFrame({'date':dates, 'team':1, 'player':1, 'elo': elo11, 'name': p11, 'id':id, 'win':win})
+df = df.append(pd.DataFrame({'date':dates, 'team':1, 'player':2, 'elo': elo12, 'name'  : p12, 'id':id, 'win':win}))
+df = df.append(pd.DataFrame({'date':dates, 'team':2, 'player':1, 'elo': elo21, 'name'  : p21, 'id':id, 'win':win}))
+df = df.append(pd.DataFrame({'date':dates, 'team':2, 'player':2, 'elo': elo22, 'name'  : p22, 'id':id, 'win':win}))
 df.sort_values(by='date', inplace=True)
 df['date'] = df['date'].apply(lambda x: x.date())
+df = df.query("name != 'NA'")
 
-is_p11 = df['t1'] & df['p11']
-is_p12 = df['t1'] & -df['p11']
-is_p21 = df['t2'] & df['p21']
-is_p22 = df['t2'] & -df['p21']
+df_player_team = df[df.name==player.full_name][['id', 'team']]
+df_player_team['player_team'] = True
 
-df['elo'] = 0
-df.loc[is_p11, 'elo'] = df['elo11'][is_p11]
-df.loc[is_p12, 'elo'] = df['elo12'][is_p12]
-df.loc[is_p21, 'elo'] = df['elo21'][is_p21]
-df.loc[is_p22, 'elo'] = df['elo22'][is_p22]
+df = pd.merge(df, df_player_team, on=['id', 'team'], how='outer')
+df.loc[df.player_team != True, 'player_team'] = False
 
-df_elo = df.groupby("date").agg({"elo" : min})
-
-# Create a trace
-trace = go.Scatter(
+# Elo plot
+df_elo = df[df.name==player.full_name].groupby("date").agg({"elo" : min})
+data = [go.Scatter(
     x = df_elo.index.values,
     y = df_elo['elo'].values
-)
-
-data = [trace]
+)]
 plot_div = po.plot(data, include_plotlyjs=False, output_type='div')
+
+# Rivals plot
+df_rivals = (df[df.player_team==False].groupby("name")
+    .agg({"name" : "count", "win":sum})
+    .rename(columns={"name":"count", "win":"wins"}))
+df_rivals['win_perc'] = round(100 * df_rivals['wins'] / df_rivals['count'])
+
+data = [go.Bar(
+            x=df_rivals['count'].values,
+            y=df_rivals.index.values,
+            text =df_rivals['win_perc'].values,
+            orientation = 'h'
+)]
+layout = go.Layout(
+  margin={"l":200})
+fig = go.Figure(data=data, layout=layout)
+plot_rivals = po.plot(fig, include_plotlyjs=False, output_type='div')
